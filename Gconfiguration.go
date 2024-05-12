79c340ec241ca4c1f903c4e83d3d65ad567857ae
Santiago Canal <santycanal_06@hotmail.com>
2024-05-12 10:38:22 -0500

    new version of gvconfiguration using ins_log library

--- Gconfiguration.go
+++ Gconfiguration.go
@@ -1,13 +1,18 @@
 package Gconfiguration
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/scch94/Gconfiguration/constants"
+	"github.com/scch94/ins_log"
 )
+
+//lint:ignore SA1029 "Using built-in type string as key for context value intentionally"
+var ctx = context.WithValue(context.Background(), "packageName", "configuration")
 
 // ConfigurationsModel es una interfaz para cualquier modelo de configuración.
 type ConfigurationsModel interface {
@@ -16,35 +21,38 @@ type ConfigurationsModel interface {
 
 // Gconfiguration carga la configuración desde un archivo JSON y devuelve un modelo de configuración.
 func GetConfig(configurationModel ConfigurationsModel) error {
-	fmt.Println("Iniciando Gconfiguration. Versión:", version())
+	ins_log.SetService("Gconfiguration")
+	ins_log.Infof(ctx, "starting gconfiguration Versión:", version())
 
 	// Obtener la ruta del ejecutable
 	exePath, err := os.Executable()
 	if err != nil {
-		return fmt.Errorf("error al obtener la ruta del ejecutable: %v", err)
+		ins_log.Fatalf(ctx, "error trying to get the file path  :", err)
+		return fmt.Errorf("error trying to get the file path  : %v", err)
 	}
 	exeDir := filepath.Dir(exePath)
 
 	// Construir la ruta al archivo de configuración
 	configFilePath := filepath.Join(exeDir, constants.CONFIG_FILE)
-	fmt.Println("Ubicación del archivo de configuración:", configFilePath)
+	ins_log.Infof(ctx, "ubication of the configurationfile", configFilePath)
 
 	// Leer el contenido del archivo de configuración
 	configData, err := os.ReadFile(configFilePath)
 	if err != nil {
-		fmt.Println("error al leer el archivo de configuracion :", err)
-		return fmt.Errorf("error al leer el archivo de configuración: %v", err)
+		ins_log.Fatalf(ctx, "problem when we try to reed the configuration file :", err)
+		return fmt.Errorf("problem when we try to reed the configuration file : %v", err)
 	}
 
 	// Decodificar el JSON en la estructura de configuración adecuada
 	//var configurationStruct structs.MicropagosConfiguration
 	err = json.Unmarshal(configData, configurationModel)
 	if err != nil {
-		return fmt.Errorf("el archivo config.json no coincide con la estructura %s, revisa el archivo nuevamente: %v", configurationModel, err)
+		ins_log.Fatalf(ctx, "the config.json file does not match the structure %s, please review the file again:", err)
+		return fmt.Errorf("the config.json file does not match the structure %s, please review the file again: %v", configurationModel, err)
 	}
 
-	fmt.Println("acabamos de obtener la configuracion")
-	fmt.Println("este es el valor de la config", configurationModel.ConfigurationString())
+	ins_log.Info(ctx, "ending to get the configuration")
+	ins_log.Tracef(ctx, "this is the data that we have in the configuration", configurationModel.ConfigurationString())
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/scch94/Gconfiguration
 
 go 1.22.2
+
+require github.com/scch94/ins_log v0.0.0-20240511034327-ce817514df2e
+
+require github.com/google/uuid v1.6.0 // indirect
